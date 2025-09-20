d191af7beea3b15cafecddbe1068c1d93801337a
Roel Villadiego <villadiegoroel92@gmail.com>
2025-09-20 23:13:47 +0800

    feat(csrf): Add CSRF token verification middleware to the HTTP kernel
    - Integrated the VerifyCsrfToken middleware into the HTTP kernel to enhance security by protecting against cross-site request forgery attacks.

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -50,6 +50,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
